70c770407642dc300e65135965a13e6e70bbbc0b
Hosmel Quintana <hosmelq@gmail.com>
2019-03-07 16:41:03 -0600

    tweak mark starts as paid

--- database/migrations/2019_02_06_203613_create_events_table.php
+++ database/migrations/2019_02_06_203613_create_events_table.php
@@ -15,7 +15,7 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->dateTime('date')->nullable();
+            $table->date('date')->nullable();
             $table->string('name');
             $table->timestamps();
         });
